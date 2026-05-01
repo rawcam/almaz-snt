4969a87cc3b931f1e73bb4f3af9adf5f7cf1e75e
rawcam <rawcam@ya.ru>
2026-05-01 22:25:47 +0300

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        onest: ['Onest', 'sans-serif'],
+      },
+      colors: {
+        almond: '#fafbf8',
+        dark: '#1e1e1e',
+        'green-deep': '#1b4d3e',
+        gold: '#b89b5e',
+        'gold-light': '#e9dbbd',
+      },
+    },
+  },
+  plugins: [],
+}

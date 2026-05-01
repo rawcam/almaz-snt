85f76d1bc637396318aea643c137cc65ea9a8e7f
rawcam <rawcam@ya.ru>
2026-05-01 23:19:51 +0300

    Update AnimatedBackground.jsx

--- src/components/AnimatedBackground.jsx
+++ src/components/AnimatedBackground.jsx
@@ -3,7 +3,6 @@ import { motion } from 'framer-motion'
 export default function AnimatedBackground() {
   return (
     <div className="absolute inset-0 z-0 overflow-hidden bg-gradient-to-br from-[#e0e6d0] via-[#f5f0e6] to-[#d4c9a8]">
-      {/* Плавающие частицы */}
       <div className="absolute inset-0">
         {[...Array(20)].map((_, i) => (
           <motion.div

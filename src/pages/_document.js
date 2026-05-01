3674b49dd18bf602313ea339d8388c448b192d82
rawcam <rawcam@ya.ru>
2026-05-02 00:13:24 +0300

    Update _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -8,6 +8,10 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Onest:wght@300;400;500;600;700&display=swap"
           rel="stylesheet"
         />
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
+        />
       </Head>
       <body>
         <Main />

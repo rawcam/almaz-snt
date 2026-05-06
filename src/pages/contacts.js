ae784dda1be8aae737df6bb045bf2d017daabd45
rawcam <rawcam@ya.ru>
2026-05-06 13:18:19 +0300

    Update contacts.js

--- src/pages/contacts.js
+++ src/pages/contacts.js
@@ -1,12 +1,10 @@
 // src/pages/contacts.js
 import Navbar from '../components/Navbar'
-import AnimatedBackgroundLight from '../components/AnimatedBackgroundLight'
 import { motion } from 'framer-motion'
 
 export default function Contacts() {
   return (
-    <div className="min-h-screen bg-almond relative">
-      <AnimatedBackgroundLight opacity={0.5} />
+    <div className="min-h-screen relative">
       <Navbar />
       <div className="container mx-auto px-4 py-20 relative z-10">
         <motion.div

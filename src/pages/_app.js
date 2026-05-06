f2bcc6b15eca98d85a573f0f13d28b9bbdbaab00
rawcam <rawcam@ya.ru>
2026-05-06 15:31:16 +0300

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,16 +1,6 @@
 // src/pages/_app.js
 import '../styles/globals.css'
-import { useRouter } from 'next/router'
-import AnimatedBackgroundLight from '../components/AnimatedBackgroundLight'
 
 export default function App({ Component, pageProps }) {
-  const router = useRouter()
-  const isHomePage = router.pathname === '/'
-
-  return (
-    <div style={{ position: 'relative', minHeight: '100vh' }}>
-      {!isHomePage && <AnimatedBackgroundLight opacity={0.5} />}
-      <Component {...pageProps} />
-    </div>
-  )
+  return <Component {...pageProps} />
 }
